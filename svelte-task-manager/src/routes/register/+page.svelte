f04e647351ed518926ea0c4f62d65aa96a2f5594
shweta <shwetachaurasia98@gmail.com>
2024-06-16 15:30:14 +0530

    chore: Update registration endpoint URL to use HTTPS

--- svelte-task-manager/src/routes/register/+page.svelte
+++ svelte-task-manager/src/routes/register/+page.svelte
@@ -5,7 +5,7 @@
     let password = '';
 
     async function register() {
-        const response = await fetch('http://localhost:3000/register', {
+        const response = await fetch('https://nagp-svelte-assignment-sigma.vercel.app/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
